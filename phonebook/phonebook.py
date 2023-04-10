ff3011341d5b7d79e01ea39cbd4b8c24f4509b51
Kushal Bogati <kushal.bogati.5@gmail.com>
2023-04-10 21:55:12 +0545

    adding get_name() function

--- phonebook/phonebook.py
+++ phonebook/phonebook.py
@@ -1,26 +1,30 @@
 # contact book using dictonaries
 # user input to register new search or edit 
 
+def get_name():
+    return input("Please enter the name ").lower()
+
 contacts = { "ram":1234567890 ,"shyam": 2121212121 ,"hari": 1234567341}
 while True:
     print("enter the thing you want to do ") 
     a =  int(input( "1 for register , 2 for search , 3 for edit , 4 for show , 5 for delete\n" ))
 
     if a ==1:
-        name = input ( " enter the name you want to register ").lower()
+        # name = input ( " enter the name you want to register ").lower()
+        name = get_name()
         phone = int( input( f" enter the number of { name } "))
         contacts[name] = phone
         print (contacts)
 
     elif a == 2:
-        search = input ( "enter the name you want to search number of ").lower()
+        search = get_name()
         if search in contacts.keys() :
             print( f" The phone number of { search.capitalize()} is { contacts[search]}")
         else:
             print("The name do not exist enter the name again ")
 
     elif a == 3:
-        edit = input ("enter the name of number you want to edit").lower()
+        edit = get_name()
         if edit in contacts.keys() :
             num = int (input( " enter the phone number "))
             contacts[edit] = num
@@ -33,7 +37,7 @@ while True:
             print(f"\t\t {k} \t\t\t | \t\t {v}")
 
     elif a == 5:
-        delete = input ("enter the name of number you want to delete ").lower()
+        delete = get_name()
         if delete in contacts.keys() :
             del ( contacts[delete] )
             print ( f"contacts of { delete .capitalize() } deleted succesfully ")
